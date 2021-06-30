1d0e2e6b1b9af94951ff4717b23f5f45c23e5318
hrdipto <=>
2021-06-30 20:44:51 +0600

    Final Update

---
+++ Project 1/Readme.md
@@ -0,0 +1,25 @@
+# IEEE-CIS Fraud Detection
+
+## Description
+
+In this Kaggle competition, you’ll benchmark machine learning models on a challenging large-scale dataset. The data comes from Vesta's real-world e-commerce transactions and contains a wide range of features from device type to product features. You also have the opportunity to create new features to improve your results.
+
+### Dependencies
+
+- Python 3, NumPy, Pandsas, Jupyter Notebook, XGBoostClassifier
+
+### Installing and Executing Program
+
+- Download the dataset from [kaggle](https://www.kaggle.com/c/ieee-fraud-detection/data)
+- Clone the git repo
+- Navigate to the project 1 directory
+- If your memory is low, reduce the size of the dataset using data_reducer.py
+- Open Fraud Detector.ipynb to train and use the model
+
+## Authors
+
+[Kazi Habibur Rahman Dipto](https://www.linkedin.com/in/hrdipto/)
+
+## License
+
+This project is licensed under the [Apache License 2.0] License - see the LICENSE.md file for details
--- Project 1/data_reducer.py
+++ Project 1/data_reducer.py
@@ -67,6 +67,67 @@ test_transaction_data = reduce_mem_usage(test_transaction_data)
 test_identity_data = reduce_mem_usage(test_identity_data)
 
 
+emails = {'gmail': 'google', 'att.net': 'att', 'twc.com': 'spectrum', 'scranton.edu': 'other', 'optonline.net': 'other', 'hotmail.co.uk': 'microsoft', 'comcast.net': 'other', 'yahoo.com.mx': 'yahoo', 'yahoo.fr': 'yahoo', 'yahoo.es': 'yahoo', 'charter.net': 'spectrum', 'live.com': 'microsoft', 'aim.com': 'aol', 'hotmail.de': 'microsoft', 'centurylink.net': 'centurylink', 'gmail.com': 'google', 'me.com': 'apple', 'earthlink.net': 'other', 'gmx.de': 'other', 'web.de': 'other', 'cfl.rr.com': 'other', 'hotmail.com': 'microsoft', 'protonmail.com': 'other', 'hotmail.fr': 'microsoft', 'windstream.net': 'other', 'outlook.es': 'microsoft', 'yahoo.co.jp': 'yahoo', 'yahoo.de': 'yahoo', 'servicios-ta.com': 'other', 'netzero.net': 'other', 'suddenlink.net': 'other', 'roadrunner.com': 'other', 'sc.rr.com': 'other', 'live.fr': 'microsoft', 'verizon.net': 'yahoo', 'msn.com': 'microsoft', 'q.com': 'centurylink', 'prodigy.net.mx': 'att', 'frontier.com': 'yahoo', 'anonymous.com': 'other', 'rocketmail.com': 'yahoo', 'sbcglobal.net': 'att', 'frontiernet.net': 'yahoo', 'ymail.com': 'yahoo', 'outlook.com': 'microsoft', 'mail.com': 'other', 'bellsouth.net': 'other', 'embarqmail.com': 'centurylink', 'cableone.net': 'other', 'hotmail.es': 'microsoft', 'mac.com': 'apple', 'yahoo.co.uk': 'yahoo', 'netzero.com': 'other', 'yahoo.com': 'yahoo', 'live.com.mx': 'microsoft', 'ptd.net': 'other', 'cox.net': 'other', 'aol.com': 'aol', 'juno.com': 'other', 'icloud.com': 'apple'}
+us_emails = ['gmail', 'net', 'edu']
+#https://www.kaggle.com/c/ieee-fraud-detection/discussion/100499#latest_transaction_data-579654
+for c in ['P_emaildomain', 'R_emaildomain']:
+    train_transaction_data[c + '_bin'] = train_transaction_data[c].map(emails)
+    test_transaction_data[c + '_bin'] = test_transaction_data[c].map(emails)
+    
+    train_transaction_data[c + '_suffix'] = train_transaction_data[c].map(lambda x: str(x).split('.')[-1])
+    test_transaction_data[c + '_suffix'] = test_transaction_data[c].map(lambda x: str(x).split('.')[-1])
+    
+    train_transaction_data[c + '_suffix'] = train_transaction_data[c + '_suffix'].map(lambda x: x if str(x) not in us_emails else 'us')
+    test_transaction_data[c + '_suffix'] = test_transaction_data[c + '_suffix'].map(lambda x: x if str(x) not in us_emails else 'us')
+
+
+
+
+
+
+numerical = [col for col in numerical if col in train_transaction_data.columns]
+categorical = [col for col in categorical if col in train_transaction_data.columns]
+
+
+def nan2mean(df):
+    for x in list(df.columns.values):
+        if x in numerical:
+            #print("___________________"+x)
+            #print(df[x].isna().sum())
+            df[x] = df[x].fillna(0)
+           #print("Mean-"+str(df[x].mean()))
+    return df
+train_transaction_data=nan2mean(train_transaction_data)
+test_transaction_data=nan2mean(test_transaction_data)
+
+
+category_counts = {}
+for f in categorical:
+    train_transaction_data[f] = train_transaction_data[f].replace("nan", "other")
+    train_transaction_data[f] = train_transaction_data[f].replace(np.nan, "other")
+    test_transaction_data[f] = test_transaction_data[f].replace("nan", "other")
+    test_transaction_data[f] = test_transaction_data[f].replace(np.nan, "other")
+    lbl = preprocessing.LabelEncoder()
+    lbl.fit(list(train_transaction_data[f].values) + list(test_transaction_data[f].values))
+    train_transaction_data[f] = lbl.transform(list(train_transaction_data[f].values))
+    test_transaction_data[f] = lbl.transform(list(test_transaction_data[f].values))
+    category_counts[f] = len(list(lbl.classes_)) + 1
+# train_transaction_data = train_transaction_data.reset_index()
+# test_transaction_data = test_transaction_data.reset_index()
+
+
+
+for column in numerical:
+    scaler = StandardScaler()
+    if train_transaction_data[column].max() > 100 and train_transaction_data[column].min() >= 0:
+        train_transaction_data[column] = np.log1p(train_transaction_data[column])
+        test_transaction_data[column] = np.log1p(test_transaction_data[column])
+    scaler.fit(np.concatenate([train_transaction_data[column].values.reshape(-1,1), test_transaction_data[column].values.reshape(-1,1)]))
+    train_transaction_data[column] = scaler.transform(train_transaction_data[column].values.reshape(-1,1))
+    test_transaction_data[column] = scaler.transform(test_transaction_data[column].values.reshape(-1,1))
+
+
+
 train_transaction_data.to_pickle('train_transaction_data.pkl')
 train_identity_data.to_pickle('train_identity_data.pkl')
 test_transaction_data.to_pickle('test_transaction_data.pkl')
